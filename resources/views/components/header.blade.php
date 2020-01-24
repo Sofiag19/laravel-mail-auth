c058f2b937c04e243bece3797cc512a848590efa
Sofiag19 <sofia.giacometti.19@gmail.com>
2020-01-24 22:59:29 +0100

    modifiche css

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,5 @@
 <header>
-  <a href="{{ route('doc.index') }}">NOWZARADAN CLINIC</a>
-  <nav class="my_nav">
-    <a href="{{ route('doc.create') }}">ADD NEW MD</a>
-  </nav>
+  <div class="title"><a href="{{ route('doc.index') }}">NOWZARADAN CLINIC</a></div>
+  <div><a href="{{ route('doc.create') }}">ADD NEW MD</a></div>
+
 </header>
